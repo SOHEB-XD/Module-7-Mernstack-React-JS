dd9a307c3dac5e29801ebcf3212e0369f288f68c
SOHEB... <112369546+SOHEB-XD@users.noreply.github.com>
2025-09-20 14:02:34 +0530

    Add files via upload

---
+++ Lab-Assignment/React-intro/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import './App.css';
+
+// This is the component we are creating
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, React!</h1>
+    </div>
+  );
+}
+
+// This line makes the App component available to be imported in other files (like index.js)
+export default App;
